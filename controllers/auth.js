3726459a5da0974295b3ac0c48021b43eb7d18d8
inna <innafeldman01@gmail.com>
2021-03-27 12:51:09 +0300

    user registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 // Controller of authorization
-
+const bcrypt = require('bcryptjs');//To encrypt user's password, to protect user's passw.
 const User = require('../models/User');
 
 module.exports.login = function(req, res){
@@ -11,12 +11,32 @@ module.exports.login = function(req, res){
      })
 }
 
-module.exports.register = function(req, res){
+module.exports.register = async function(req, res){
     //email password
+  ///If email and password already exist
+  const candidate = await User.findOne({email: req.body.email}) // (async await) - Code is waiting for execution of code from functionUser.findOne({email: req.body.email} and then continue ti execute rest of code.
+
+  if (candidate) {
+      //If user exist, show an error
+      //https://httpstatuses.com/
+      res.status(409).json({
+          message: 'This email already exist. Try another one.' 
+      })
+  }else {
+      // If there is no such user, create a user
+    const salt = bcrypt.genSaltSync(10);// To encrypt passw.
+    const password = req.body.password; //Passw. from user
+
     const user = new User({
         email: req.body.email,
-        password: req.body.password
+        password: bcrypt.hashSync(password, salt)//When we create user instade of user's passw. we'll get hash
     })
 
-    user.save().then(() => console.log('User is created'));
+    try {
+        await user.save();
+        res.status(201).json(user)
+    }catch(e) {
+        //TO process Error
+    }
+  }
 }
