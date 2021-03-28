057fab26938efecaea96080cbca701c3539ac88e
inna <innafeldman01@gmail.com>
2021-03-28 16:18:44 +0300

    form validation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
@@ -7,6 +8,8 @@ import {AppRoutingModule} from './app-routing.module';
 import { AuthLayoutComponent } from './components/shared/layouts/auth-layout/auth-layout.component';
 import { SiteLayoutComponent } from './components/shared/layouts/site-layout/site-layout.component';
 import { SignUpPageComponent } from './components/sign-up-page/sign-up-page.component';
+import { ConsoleLogPipe } from './pipes/console-log.pipe';
+
 
 @NgModule({
   declarations: [
@@ -14,11 +17,14 @@ import { SignUpPageComponent } from './components/sign-up-page/sign-up-page.comp
     LoginPageComponent,
     AuthLayoutComponent,
     SiteLayoutComponent,
-    SignUpPageComponent
+    SignUpPageComponent,
+    ConsoleLogPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
